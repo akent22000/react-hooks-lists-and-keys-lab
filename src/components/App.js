0bf7edc0094f836099c8482869a7f70683b64e3b
amandalocher <akent22000@gmail.com>
2024-09-15 12:08:46 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
+import ProjectItem from "./ProjectItem";
 import user from "../data/user";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Home username={user.name} city={user.city} color={user.color} />
       <About bio={user.bio} links={user.links} />
       <ProjectList projects={user.projects} />
+
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,19 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkElements = links.map((link) => {
+    return (
+      <li key={link}>
+        {link}
+      </li>
+    );
+  });
+  // return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    <a href="#home">home</a>
+    <a href="#about">about</a>
+    <a href="#projects">projects</a>
+  </nav>;
 }
 
 export default NavBar;
